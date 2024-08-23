3ef7a578db78c7c57b07ee90712579de8b2ec070
emzet93 <maciej.m.janawa@gmail.com>
2024-08-23 11:13:41 +0200

    use shader for backdrop

--- components/Modal/ModalBackdrop.tsx
+++ components/Modal/ModalBackdrop.tsx
@@ -1,127 +1,65 @@
-import { Canvas, Line } from "@shopify/react-native-skia";
+import { Canvas, Fill, Shader, Skia } from "@shopify/react-native-skia";
 import React, { FC, useEffect, useState } from "react";
-import { UnistylesRuntime, useStyles } from "react-native-unistyles";
+import { UnistylesRuntime } from "react-native-unistyles";
+
+import { ThemeFilter } from "@/components/ImageFilters";
 
 const { width, height } = UnistylesRuntime.screen;
 
 const frames = [
-  {
-    verticalLineWidth: 0.3,
-    horizontalLineWidth: 0.3,
-    verticalSpacing: 1,
-    horizontalSpacing: 1,
-  },
-  {
-    verticalLineWidth: 0.5,
-    horizontalLineWidth: 0.5,
-    verticalSpacing: 1,
-    horizontalSpacing: 1,
-  },
-  {
-    verticalLineWidth: 0.75,
-    horizontalLineWidth: 0.5,
-    verticalSpacing: 1,
-    horizontalSpacing: 1,
-  },
-  {
-    verticalLineWidth: 0.5,
-    horizontalLineWidth: 0.75,
-    verticalSpacing: 1,
-    horizontalSpacing: 1,
-  },
-  {
-    verticalLineWidth: 1,
-    horizontalLineWidth: 1,
-    verticalSpacing: 1,
-    horizontalSpacing: 1,
-  },
+  { x: 8, y: 4 },
+  { x: 2, y: 4 },
+  { x: 2, y: 2 },
+  { x: 1, y: 2 },
+  { x: 1, y: 1 },
 ];
 
-export const ModalBackdrop: FC = React.memo(() => {
-  const { theme } = useStyles();
+const source = Skia.RuntimeEffect.Make(`
+uniform float xDensity;
+uniform float yDensity;
 
-  const [frame, setFrame] = useState(frames[0]);
+vec4 main(vec2 pos) {
+  if (int(mod(pos.x, xDensity + 1)) == 0) {
+    return vec4(0, 0, 0, 1);
+  }
+  if (int(mod(pos.y, yDensity + 1)) == 0) {
+    return vec4(0, 0, 0, 1);
+  }
+  
+  return vec4(0, 0, 0, 0);
+}`)!;
+
+export const ModalBackdrop: FC = React.memo(() => {
+  const [density, setDensity] = useState(frames[0]);
 
   useEffect(() => {
     let index = 0;
 
     const id = setInterval(() => {
-      setFrame((_frame) => {
-        if (index === frames.length - 1) {
-          clearInterval(id);
-          return _frame;
-        }
+      if (index === frames.length - 1) {
+        clearInterval(id);
+        return;
+      }
 
-        index += 1;
-        return frames[index];
-      });
-    }, 50);
+      index += 1;
+
+      setDensity(frames[index]);
+    }, 75);
 
     return () => {
       clearInterval(id);
     };
   }, []);
 
-  const {
-    verticalLineWidth,
-    horizontalLineWidth,
-    verticalSpacing,
-    horizontalSpacing,
-  } = frame;
-
-  const numVerticalLines = Math.ceil(
-    width / (verticalSpacing + verticalLineWidth),
-  );
-  const numHorizontalLines = Math.ceil(
-    height / (horizontalSpacing + horizontalLineWidth),
-  );
-
-  // Create an array of vertical lines
-  const verticalLines = [];
-  for (let i = 0; i <= numVerticalLines; i++) {
-    const x = i * (verticalSpacing + verticalLineWidth);
-    verticalLines.push(
-      <Line
-        key={`v-${i}`}
-        p1={{
-          x,
-          y: 0,
-        }}
-        p2={{
-          x,
-          y: height,
-        }}
-        strokeWidth={verticalLineWidth}
-        color={theme.colors.primary}
-      />,
-    );
-  }
-
-  // Create an array of horizontal lines
-  const horizontalLines = [];
-  for (let i = 0; i <= numHorizontalLines; i++) {
-    const y = i * (horizontalSpacing + horizontalLineWidth);
-    horizontalLines.push(
-      <Line
-        key={`h-${i}`}
-        p1={{
-          x: 0,
-          y,
-        }}
-        p2={{
-          x: width,
-          y,
-        }}
-        strokeWidth={horizontalLineWidth}
-        color={theme.colors.primary}
-      />,
-    );
-  }
-
   return (
     <Canvas style={{ width, height }}>
-      {verticalLines}
-      {horizontalLines}
+      <Fill>
+        <Shader
+          source={source}
+          uniforms={{ xDensity: density.x, yDensity: density.y }}
+        />
+      </Fill>
+      <ThemeFilter width={width} height={height} />
     </Canvas>
   );
 });
